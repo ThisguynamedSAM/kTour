316a4c0b45e0fb80194b0700ea89823968ebb49b
ThisguynamedSAM <Sam.d.colon@gmail.com>
2013-11-05 15:30:20 -0500

    removed unused variables

--- src/kTour.java
+++ src/kTour.java
@@ -2,17 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Arrays;
 
-public class kTour extends JFrame{
+public class kTour extends JFrame {
 
+    private static int dim = 8; // hold dimensions of the board
+    private static int ctr = 0; // will count moves and add data to squares
     private static int[] c_loc = { 0, 0 };  // array that will hold knights current location
     private static int[] lastMove = { 0, 0 }; // used to reset location after calling getAccess method
+    private static int[][] testBoard = new int[dim][dim]; // this is a temporary board size 8X8
+    // will be removed after object -> int[][] problem is solved
+    private static int[][] access = new int[dim][dim]; // accessibility matrix
     private final static int[][] MOVES = { { 1, -2 }, { 2, -1 }, { 2, 1 }, { 1, 2 }, { -1, 2 },
             { -2, 1 }, { -2, -1 }, { -1, -2 } }; // all possible moves
-    private static int[][] testBoard = new int[8][8]; // this is a temporary board size 8X8
-    // will be removed after object -> int[][] problem is solved
-    private static int[][] access = new int[8][8]; // accessibility matrix
-    private static int ctr = 0; // will count moves and add data to squares
-    private static int dim = 8; // hold dimensions of the board
     private static boolean error = false; // in case everything isn't perfect
 
     /* for GUI */
@@ -20,20 +20,16 @@ public class kTour extends JFrame{
     private static JLabel[][] board;
     private JPanel boardPanel;
 
-    /*
-        add method that would look ahead in case of a tie in adj matrix
-            this method will make a new adj matrix for each possible move
-     */
-
-    // default constructor
+    // constructor
     kTour() {
-        // c_loc initialization can be change for user input
-        c_loc = new int[] { 0, 0};
-        Board b = new Board(8); // creates object of
-        Board.reset( access ); // reset the board before using
-        getAccess(); // computes access for each square
-        setLoc( 0, 0 ); // returns back to starting position
+        //    Things to add
+        // create object board b
+        // use b.temp instead of testboard
+        // add look ahead method for access matrix
+        // create new constructor that will iterate through each starting position and complete
+        // the tour
 
+        /* GUI */
         setTitle( "Knights Tour" );
         setLayout( new BorderLayout() );
         setSize( 500, 500 );
@@ -47,7 +43,7 @@ public class kTour extends JFrame{
 
         // color the labels accordingly
         Color c;
-        for(int i = 0; i < dim; i++)
+        for(int i = 0; i < dim; i++) {
             for(int k = 0; k < dim; k++) {
                 board[i][k] = new JLabel( "", SwingConstants.CENTER );
                 if(i % 2 == 0) // even rows
@@ -66,13 +62,16 @@ public class kTour extends JFrame{
                 board[i][k].setBackground( c ); // set label color
                 boardPanel.add( board[i][k] );
             }
-        // resets the board to 0
-        Board.reset( testBoard ); // assigns 0's to the board throughout the grid
-        testBoard[c_loc[0]][c_loc[1]] = ++ctr; // assigns first square with data
-        start();
+        }
+        /* end GUI */
+
+        testBoard[0][0] = ++ctr; // assigns starting position with 1
+        start(); // starts tour
         fillGUI(); // copies contents of testboard to GUI
+
         // combines all layers to container
         pane.add( boardPanel, BorderLayout.CENTER );
+
         // Display and lock the window.
         setVisible( true );
         setResizable( false );
@@ -80,11 +79,16 @@ public class kTour extends JFrame{
 
     private static void fillGUI() {
         // after tour is run this method copies results to GUI template
-        for ( int i = 0; i < dim; i++)
-            for (int j = 0; j < dim; j++)
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                // if board is not complete
+                if(testBoard[i][j] == 0)
+                    // do not add 0 to GUI
+                    continue;
                 board[i][j].setText( Integer.toString( testBoard[i][j] ) );
+            }
+        }
     }
-
 
     private static void start() {
         // This method gets a legal move for each square
@@ -120,13 +124,14 @@ public class kTour extends JFrame{
           2 3 4 4 4 4 3 2
         */
         // compute access for each square
+
         for(int i = 0; i < dim; i++) {
             for(int j = 0; j < dim; j++) {
-                if(testBoard[i][j] == 0) {
+                if(testBoard[i][j] == 0) { // 0 meaning empty
                     setLoc( i, j );
                     computeAccess(); // compute access for location set above
                 } else {
-                    access[i][j] = -1;
+                    access[i][j] = 0; // -1 for not accessible
                 }
             }
         }
@@ -138,7 +143,7 @@ public class kTour extends JFrame{
         int m = 0; // m for amount of moves
         for(int i = 0; i < 8; i++) // 8 being the max combinations of moves
             if(isLegal( combine( MOVES[i] ) )) // if legal move is found
-                ++m; // increment
+                m++; // increment
 
         access[c_loc[0]][c_loc[1]] = m; // sets current square to m
     }
@@ -181,8 +186,8 @@ public class kTour extends JFrame{
     }
 
     private static int[] compare(int[] a, int[] b) {
-    // this is wrong but it works when change < to > program does not finish
-        if ( access[a[0]][a[1]] <= access[b[0]][b[1]] ) {
+        // this is wrong but it works when change < to > program does not finish
+        if(access[a[0]][a[1]] >= access[b[0]][b[1]]) {
             return b;
         }
         return a;
@@ -191,7 +196,7 @@ public class kTour extends JFrame{
     private static boolean isLegal(int move[]) {
         // This method first checks if move is greater than or equal to 0 and less than 8 (is within grid)
         // Than checks if that square already contains data
-        if(( move[0] >= 0 && move[0] < 8 ) && ( move[1] >= 0 && move[1] < 8 )) { // within bounds?
+        if(( move[0] >= 0 && move[0] < dim ) && ( move[1] >= 0 && move[1] < dim )) { // within bounds?
             if(testBoard[move[0]][move[1]] == 0) // is empty
                 return true;
         }
@@ -206,7 +211,6 @@ public class kTour extends JFrame{
 
     private static int[] combine(int move[]) {
         // This method will take current location and combine it with potential move
-        // Method will be ambiguous if their already exist a method in the jdk that accomplishes this.
         int[] a = { c_loc[0] + move[0],
                 c_loc[1] + move[1] };
         return a;
@@ -214,6 +218,6 @@ public class kTour extends JFrame{
 
     public static void main(String[] args) {
 
-        new kTour(); // calls constructor of kTour
+        new kTour();
     }
 }
